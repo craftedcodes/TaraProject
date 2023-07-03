b79aad2f2e8bc053e1cb24bad9ab63363a1af9db
Lea <craftedcodesberlin@gmail.com>
2023-07-03 22:19:32 +0200

    Edited GratitudeJournalFragment.kt, fragment_animation.xml, fragment_affirmation.xml, added fragment_gratitude_journal.xml, strings, styles

--- app/src/main/java/com/example/abschlussaufgabe/ui/GratitudeJournalFragment.kt
+++ app/src/main/java/com/example/abschlussaufgabe/ui/GratitudeJournalFragment.kt
@@ -5,56 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.abschlussaufgabe.R
+import com.example.abschlussaufgabe.databinding.FragmentGratitudeJournalBinding
+import java.util.Calendar
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [GratitudeJournalFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class GratitudeJournalFragment : Fragment() {
-	// TODO: Rename and change types of parameters
-	private var param1: String? = null
-	private var param2: String? = null
-	
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-		arguments?.let {
-			param1 = it.getString(ARG_PARAM1)
-			param2 = it.getString(ARG_PARAM2)
-		}
-	}
+	private lateinit var binding: FragmentGratitudeJournalBinding
 	
 	override fun onCreateView(
 		inflater: LayoutInflater, container: ViewGroup?,
 		savedInstanceState: Bundle?
 	): View? {
-		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_gratitude_journal, container, false)
-	}
-	
-	companion object {
-		/**
-		 * Use this factory method to create a new instance of
-		 * this fragment using the provided parameters.
-		 *
-		 * @param param1 Parameter 1.
-		 * @param param2 Parameter 2.
-		 * @return A new instance of fragment GratitudeJournalFragment.
-		 */
-		// TODO: Rename and change types and number of parameters
-		@JvmStatic
-		fun newInstance(param1: String, param2: String) =
-			GratitudeJournalFragment().apply {
-				arguments = Bundle().apply {
-					putString(ARG_PARAM1, param1)
-					putString(ARG_PARAM2, param2)
-				}
-			}
+		binding = DataBindingUtil.inflate(inflater, R.layout.fragment_gratitude_journal, container, false)
+		
+		val datePicker = binding.datePicker
+		val calendar = Calendar.getInstance()
+		val year = calendar.get(Calendar.YEAR)
+		val month = calendar.get(Calendar.MONTH)
+		val day = calendar.get(Calendar.DAY_OF_MONTH)
+		
+		datePicker.init(year, month, day) { view, selectedYear, selectedMonth, selectedDay ->
+			// Hier kommt die Funktionalität des Datepickers hin.
+		}
+		
+		return binding.root
 	}
 }
