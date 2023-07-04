b2b2aee7b1e61366c2ada461f1919b3c4365e56f
Lea <craftedcodesberlin@gmail.com>
2023-07-04 10:21:13 +0200

    Removed DatePicker, added app icon

--- app/src/main/java/com/example/abschlussaufgabe/ui/GratitudeJournalFragment.kt
+++ app/src/main/java/com/example/abschlussaufgabe/ui/GratitudeJournalFragment.kt
@@ -1,6 +1,8 @@
 package com.example.abschlussaufgabe.ui
 
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,8 +12,10 @@ import com.example.abschlussaufgabe.R
 import com.example.abschlussaufgabe.databinding.FragmentGratitudeJournalBinding
 import java.util.Calendar
 
-class GratitudeJournalFragment : Fragment() {
+class GratitudeJournalFragment() : Fragment() {
 	private lateinit var binding: FragmentGratitudeJournalBinding
+	
+	
 	
 	override fun onCreateView(
 		inflater: LayoutInflater, container: ViewGroup?,
@@ -19,16 +23,7 @@ class GratitudeJournalFragment : Fragment() {
 	): View? {
 		binding = DataBindingUtil.inflate(inflater, R.layout.fragment_gratitude_journal, container, false)
 		
-		val datePicker = binding.datePicker
-		val calendar = Calendar.getInstance()
-		val year = calendar.get(Calendar.YEAR)
-		val month = calendar.get(Calendar.MONTH)
-		val day = calendar.get(Calendar.DAY_OF_MONTH)
-		
-		datePicker.init(year, month, day) { view, selectedYear, selectedMonth, selectedDay ->
-			// Hier kommt die Funktionalität des Datepickers hin.
-		}
-		
 		return binding.root
 	}
+	
 }
