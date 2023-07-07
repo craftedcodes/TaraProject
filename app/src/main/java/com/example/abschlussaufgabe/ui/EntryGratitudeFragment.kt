daa41ff578fb461f6309fd0ae7d55383961011d4
Lea <craftedcodesberlin@gmail.com>
2023-07-07 10:04:43 +0200

    Deleted unnecessary fragments, renamed fragments and adjusted layouts and navigation

--- app/src/main/java/com/example/abschlussaufgabe/ui/EntryGratitudeFragment.kt
+++ app/src/main/java/com/example/abschlussaufgabe/ui/EntryGratitudeFragment.kt
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [GratitudeTextFragment.newInstance] factory method to
+ * Use the [EntryGratitudeFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class GratitudeTextFragment : Fragment() {
+class EntryGratitudeFragment : Fragment() {
 	// TODO: Rename and change types of parameters
 	private var param1: String? = null
 	private var param2: String? = null
@@ -35,7 +35,7 @@ class GratitudeTextFragment : Fragment() {
 		savedInstanceState: Bundle?
 	): View? {
 		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_gratitude_text, container, false)
+		return inflater.inflate(R.layout.fragment_text_gratitude, container, false)
 	}
 	
 	companion object {
@@ -50,7 +50,7 @@ class GratitudeTextFragment : Fragment() {
 		// TODO: Rename and change types and number of parameters
 		@JvmStatic
 		fun newInstance(param1: String, param2: String) =
-			GratitudeTextFragment().apply {
+			EntryGratitudeFragment().apply {
 				arguments = Bundle().apply {
 					putString(ARG_PARAM1, param1)
 					putString(ARG_PARAM2, param2)
--- app/src/main/java/com/example/abschlussaufgabe/ui/JournalGratitudeFragment.kt
+++ app/src/main/java/com/example/abschlussaufgabe/ui/JournalGratitudeFragment.kt
@@ -1,19 +1,16 @@
 package com.example.abschlussaufgabe.ui
 
 import android.os.Bundle
-import android.os.Parcel
-import android.os.Parcelable
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.example.abschlussaufgabe.R
-import com.example.abschlussaufgabe.databinding.FragmentGratitudeJournalBinding
-import java.util.Calendar
+import com.example.abschlussaufgabe.databinding.FragmentJournalGratitudeBinding
 
-class GratitudeJournalFragment() : Fragment() {
-	private lateinit var binding: FragmentGratitudeJournalBinding
+class JournalGratitudeFragment() : Fragment() {
+	private lateinit var binding: FragmentJournalGratitudeBinding
 	
 	
 	
@@ -21,7 +18,7 @@ class GratitudeJournalFragment() : Fragment() {
 		inflater: LayoutInflater, container: ViewGroup?,
 		savedInstanceState: Bundle?
 	): View? {
-		binding = DataBindingUtil.inflate(inflater, R.layout.fragment_gratitude_journal, container, false)
+		binding = DataBindingUtil.inflate(inflater, R.layout.fragment_journal_gratitude, container, false)
 		
 		return binding.root
 	}
