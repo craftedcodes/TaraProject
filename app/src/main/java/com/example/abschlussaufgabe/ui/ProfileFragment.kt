13d059b2bc1b6260dd7136737ab0b36c66260f54
Lea <craftedcodesberlin@gmail.com>
2023-07-07 13:37:49 +0200

    Added new icons, id, renamed files and worked on profile ui

--- app/src/main/java/com/example/abschlussaufgabe/ui/ProfileFragment.kt
+++ app/src/main/java/com/example/abschlussaufgabe/ui/ProfileFragment.kt
@@ -1,60 +1,58 @@
 package com.example.abschlussaufgabe.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.abschlussaufgabe.R
+import com.example.abschlussaufgabe.databinding.FragmentProfileBinding
+import com.github.aachartmodel.aainfographics.aachartcreator.AAChartModel
+import com.github.aachartmodel.aainfographics.aachartcreator.AAChartType
+import com.github.aachartmodel.aainfographics.aachartcreator.AASeriesElement
+import com.github.aachartmodel.aainfographics.aaoptionsmodel.AAStyle
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileFragment : Fragment() {
-	// TODO: Rename and change types of parameters
-	private var param1: String? = null
-	private var param2: String? = null
-	
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-		arguments?.let {
-			param1 = it.getString(ARG_PARAM1)
-			param2 = it.getString(ARG_PARAM2)
-		}
-	}
+	private lateinit var binding: FragmentProfileBinding
 	
 	override fun onCreateView(
-		inflater: LayoutInflater, container: ViewGroup?,
+		inflater: LayoutInflater,
+		container: ViewGroup?,
 		savedInstanceState: Bundle?
 	): View? {
-		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_profile, container, false)
+		binding = FragmentProfileBinding.inflate(inflater, container, false)
+		return binding.root
+	}
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+		
+		val barData = arrayOf<Any>(7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6)
+		val lineData = arrayOf<Any>(3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8)
+		
+		val barSeriesElement = AASeriesElement()
+			.name("Column")
+			.color("#FFDAD6")
+			.type(AAChartType.Column)
+			.data(barData)
+		
+		val lineSeriesElement = AASeriesElement()
+			.name("Line")
+			.color("#FFE088")
+			.type(AAChartType.Line)
+			.data(lineData)
+		
+		val aaChartModel = AAChartModel()
+			.chartType(AAChartType.Column) // Diese Zeile ist wichtig, um Balken und Linien in einem Diagramm zu mischen.
+			.title(getString(R.string.your_gratitude_note_activity))
+			.backgroundColor("#00000000")
+			.dataLabelsEnabled(true)
+			.series(arrayOf(barSeriesElement, lineSeriesElement))
+			.yAxisTitle("Amount of notes per day")
+			.axesTextColor("#FFFFFF")
+			.titleStyle(AAStyle().color("#FFFFFF"))
+		
+		binding.aaChartView.aa_drawChartWithChartModel(aaChartModel)
 	}
 	
-	companion object {
-		/**
-		 * Use this factory method to create a new instance of
-		 * this fragment using the provided parameters.
-		 *
-		 * @param param1 Parameter 1.
-		 * @param param2 Parameter 2.
-		 * @return A new instance of fragment ProfileBarChartFragment.
-		 */
-		// TODO: Rename and change types and number of parameters
-		@JvmStatic
-		fun newInstance(param1: String, param2: String) =
-			ProfileFragment().apply {
-				arguments = Bundle().apply {
-					putString(ARG_PARAM1, param1)
-					putString(ARG_PARAM2, param2)
-				}
-			}
-	}
 }
