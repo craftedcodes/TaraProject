0e2d9f5013f266e938cd8c9c2cc32ba8a8d158b9
Lea <craftedcodesberlin@gmail.com>
2023-07-10 09:00:43 +0200

    Navigation plugin doesn't work

--- app/src/main/java/com/example/abschlussaufgabe/ui/HomeFragment.kt
+++ app/src/main/java/com/example/abschlussaufgabe/ui/HomeFragment.kt
@@ -5,24 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import app.rive.runtime.kotlin.RiveAnimationView
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import app.rive.runtime.kotlin.core.Loop
 import com.example.abschlussaufgabe.R
+import com.example.abschlussaufgabe.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
+	
+	private lateinit var binding: FragmentHomeBinding
+	
 	override fun onCreateView(
 		inflater: LayoutInflater, container: ViewGroup?,
 		savedInstanceState: Bundle?
 	): View? {
+		
 		// Inflate the layout for this fragment
-		val view = inflater.inflate(R.layout.fragment_home, container, false)
+		binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
 		
 		// Initialize the Rive Animation View
-		val riveView = view.findViewById<RiveAnimationView>(R.id.rive_animation_view)
+		val riveView = binding.riveAnimationView
 		riveView.setRiveResource(R.raw.tara_light)
 		riveView.play("Timeline 1", Loop.LOOP)
 		
-		return view
+		return binding.root
+	}
+	
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+		
+		binding.loginNavigationBtn.setOnClickListener {
+			findNavController().navigate(R.id.action_homeFragment_to_loginFragment)
+		}
 	}
 }
