50a65602b2b87d693466aee020ae1a5358b3217a
Lea <craftedcodesberlin@gmail.com>
2023-07-11 15:01:46 +0200

    Removed Rive implementation because of conflicts, added db, dao and data classes

--- app/src/main/java/com/example/abschlussaufgabe/data/datamodels/Entry.kt
+++ app/src/main/java/com/example/abschlussaufgabe/data/datamodels/Entry.kt
@@ -1,1 +1,28 @@
 package com.example.abschlussaufgabe.data.datamodels
+
+// Required imports for the class.
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.sql.Blob
+
+// Defining an Entity in Room. This represents a SQLite table in the database.
+@Entity(tableName = "entry")
+
+// A data class represents an Entry in the database.
+data class Entry(
+	// The PrimaryKey annotation is a must for every entity.
+	// This is to uniquely identify each entry in the database.
+	// autoGenerate property of the annotation is set to true,
+	// meaning Room will automatically generate IDs for each entry.
+	@PrimaryKey(autoGenerate = true)
+	val id: Long = 0,
+	
+	// Define a non-null field date of type String.
+	var date: String,
+	
+	// Define a nullable field text of type String. The question mark denotes it is nullable.
+	var text: String?,
+	
+	// Define a nullable field media of type Blob, which is generally used for storing binary data.
+	var media: Blob?
+)
---
+++ app/src/main/java/com/example/abschlussaufgabe/data/local/EntryDatabaseDao.kt
@@ -0,0 +1,31 @@
+package com.example.abschlussaufgabe.data.local
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.abschlussaufgabe.data.datamodels.Entry
+
+@Dao
+interface EntryDatabaseDao {
+	
+	@Insert(onConflict = OnConflictStrategy.REPLACE)
+	suspend fun insertAll(entries: List<Entry>)
+	
+	@Update
+	suspend fun update(entry: Entry)
+	
+	@Query("SELECT * FROM entry")
+	fun getAll(): LiveData<List<Entry>>
+	
+	@Query("SELECT * FROM entry WHERE id = :id")
+    fun getById(id: Long): LiveData<Entry>
+	
+	@Query("DELETE FROM entry")
+	suspend fun deleteAll()
+	
+	@Query("DELETE FROM entry WHERE id = :id")
+    suspend fun delete(id: Long)
+}
