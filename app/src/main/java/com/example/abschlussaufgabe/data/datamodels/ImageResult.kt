538ccdb1e1b2984046d271f3fa7d5caf462fd60f
Lea <craftedcodesberlin@gmail.com>
2023-07-18 15:21:04 +0200

    Found DB bug

--- app/src/main/java/com/example/abschlussaufgabe/data/datamodels/ImageResult.kt
+++ app/src/main/java/com/example/abschlussaufgabe/data/datamodels/ImageResult.kt
@@ -3,17 +3,16 @@ package com.example.abschlussaufgabe.data.datamodels
 
 // Import statement
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 // Annotation to indicate that this is an Entity class and the table name in the database will be 'image_result'
 @Entity(tableName = "image_result")
 
 // Data class declaration for ImageResult
 data class ImageResult(
+	@PrimaryKey
 	val id: Long, // Unique identifier for the image
-	val width: Int, // Width of the image
-	val height: Int, // Height of the image
 	val color: String, // Color of the image
-	val blur_hash: String, // Hash value for the blur effect of the image
 	val user: User, // User who uploaded the image
 	val urls: Urls, // URLs for the image
 )
--- app/src/main/java/com/example/abschlussaufgabe/data/datamodels/Quote.kt
+++ app/src/main/java/com/example/abschlussaufgabe/data/datamodels/Quote.kt
@@ -3,11 +3,14 @@ package com.example.abschlussaufgabe.data.datamodels
 
 // Import statement
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 // Annotation to indicate that this is an Entity class and the table name in the database will be 'quote'
 @Entity(tableName = "quote")
 
 // Data class declaration for Quote
 data class Quote(
+	@PrimaryKey(autoGenerate = true)
+	val id: Int = 0, // Primary key
 	val quote: String // The quote text
 )
