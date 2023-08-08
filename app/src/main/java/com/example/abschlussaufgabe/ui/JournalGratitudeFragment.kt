86bf06bbc5310475ef23978d0993eb01fba21895
Lea <craftedcodesberlin@gmail.com>
2023-08-08 17:02:34 +0200

    Deleted obsolete stuff

--- app/src/main/java/com/example/abschlussaufgabe/ui/JournalGratitudeFragment.kt
+++ app/src/main/java/com/example/abschlussaufgabe/ui/JournalGratitudeFragment.kt
@@ -8,7 +8,6 @@ import android.os.Build
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -159,7 +158,6 @@ class JournalGratitudeFragment() : Fragment() {
 			
 			viewModel.getEntriesByDataRange(from, to).observe(viewLifecycleOwner) { entries ->
 				// Update the data in the adapter.
-				Log.e("Irgendwas.", entries.size.toString())
 				binding.outerRvGratitudeJournal.adapter = EntryAdapter(requireContext(), entries, entryPref, countPref)
 			}
 		}
