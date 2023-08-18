407839ea74936b0ff0be71188d63658d04b1b3e1
Lea <craftedcodesberlin@gmail.com>
2023-08-18 13:00:26 +0200

    Try to add chart view
    changed files:
    - fragment_profile.xml
    - ProfileFragment.kt
    - strings.xml

--- app/src/main/java/com/example/abschlussaufgabe/ui/ProfileFragment.kt
+++ app/src/main/java/com/example/abschlussaufgabe/ui/ProfileFragment.kt
@@ -2,15 +2,12 @@ package com.example.abschlussaufgabe.ui
 
 // Required imports for the class.
 import android.content.Context
-import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.MutableLiveData
 import androidx.navigation.fragment.findNavController
 import com.github.aachartmodel.aainfographics.aachartcreator.AAChartModel
 import com.github.aachartmodel.aainfographics.aachartcreator.AAChartType
@@ -22,15 +19,16 @@ import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 import com.schubau.tara.R
 import com.schubau.tara.databinding.FragmentProfileBinding
+import java.time.Duration
 import java.time.LocalDate
-import java.util.Calendar
+import java.time.LocalDateTime
 
 // Constants for SharedPreferences keys.
 private const val AVATAR_PREFS = "avatar_prefs"
 private const val CONTACT_PREFS = "contact_prefs"
 
 // TAG for the class.
-private const val TAG = "ProfileFragment"
+private const val PROFILE_TAG = "ProfileFragment"
 
 /**
  * A Fragment representing the user's profile screen.
@@ -47,14 +45,10 @@ class ProfileFragment : Fragment() {
 	// Holds the FirebaseAuth instance.
 	private lateinit var auth: FirebaseAuth
 	
-	// Create an instance of SharedPreferences.
-	private val countPref by lazy { activity?.getSharedPreferences("count_preferences", Context.MODE_PRIVATE) }
+	private var lastFetchTime: LocalDateTime? = null
 	
-	// LiveData to hold bar chart data.
-	private val barDataLiveData = MutableLiveData<Array<Any>>()
-	
-	// LiveData to hold line chart data.
-	private val lineDataLiveData = MutableLiveData<Array<Any>>()
+	private val barChart: MutableList<Int> = mutableListOf()
+	private val lineChart: MutableList<Int> = mutableListOf()
 	
 	/**
 	 * Called to have the fragment instantiate its user interface view.
@@ -90,22 +84,9 @@ class ProfileFragment : Fragment() {
 		// Call the super method to ensure proper initialization of the view.
 		super.onViewCreated(view, savedInstanceState)
 		
-		// Generate chart data based on user's gratitude note activity.
-		barDataLiveData.observe(viewLifecycleOwner) { data ->
-			setupChartData(data, lineDataLiveData.value ?: arrayOf())
-		}
+		save30DummyDataToFirestore()
 		
-		lineDataLiveData.observe(viewLifecycleOwner) { data ->
-			setupChartData(barDataLiveData.value ?: arrayOf(), data)
-		}
-		
-		getCountEntryToFirestore()
-		
-		// Generate chart data based on user's gratitude note activity.
-		generateChartData()
-		
-		// Define and set up chart data.
-		setupChartData()
+		getCountEntryFromFirestore()
 		
 		// Set up listeners for the UI elements.
 		setUpListeners()
@@ -116,26 +97,31 @@ class ProfileFragment : Fragment() {
 	 */
 	private fun loadContactDataFromSharedPreferences() {
 		// Retrieve the shared preferences for the avatar settings.
-		val avatarSharedPreferences = requireActivity().getSharedPreferences(AVATAR_PREFS, Context.MODE_PRIVATE)
-
+		val avatarSharedPreferences =
+			requireActivity().getSharedPreferences(AVATAR_PREFS, Context.MODE_PRIVATE)
+		
 		// Retrieve the shared preferences for the contact settings.
-		val contactSharedPreferences = requireActivity().getSharedPreferences(CONTACT_PREFS, Context.MODE_PRIVATE)
-
+		val contactSharedPreferences =
+			requireActivity().getSharedPreferences(CONTACT_PREFS, Context.MODE_PRIVATE)
+		
 		// Fetch the avatar resource ID from the shared preferences. If not found, default to 'avatar01'.
 		val avatarResource = avatarSharedPreferences.getInt("selected_avatar", R.drawable.avatar01)
-
+		
 		// Fetch the contact name, number, and message from the shared preferences.
 		// Default values are provided in case the preferences do not contain the specified keys.
 		val name = contactSharedPreferences.getString("contact_name", "")
 		val number = contactSharedPreferences.getString("contact_number", "")
-		val message = contactSharedPreferences.getString("contact_message", "I am in an emotional emergency. Please call me.")
-
+		val message = contactSharedPreferences.getString(
+			"contact_message",
+			"I am in an emotional emergency. Please call me."
+		)
+		
 		// Set the avatar image, contact name, number, and message to the respective UI elements.
 		binding.avatarIv.setImageResource(avatarResource)
 		binding.nameTv.text = name
 		binding.numberTv.text = number
 		binding.messageTv.text = message
-
+		
 		// Check if both the name and number are provided.
 		// If they are, display the contact card and hide the attention card.
 		if (name != "" && number != "") {
@@ -145,10 +131,10 @@ class ProfileFragment : Fragment() {
 		
 		
 		// The onClickListener for the edit button.
-        // When clicked, the user is navigated to the emergency contact fragment.
-        binding.editBtn.setOnClickListener {
-            findNavController().navigate(ProfileFragmentDirections.actionProfileFragmentToEmergencyContactFragment())
-        }
+		// When clicked, the user is navigated to the emergency contact fragment.
+		binding.editBtn.setOnClickListener {
+			findNavController().navigate(ProfileFragmentDirections.actionProfileFragmentToEmergencyContactFragment())
+		}
 		
 		// The onClickListener for the delete button.
 		// When clicked, the contact details are being deleted from SharedPreferences.
@@ -162,8 +148,9 @@ class ProfileFragment : Fragment() {
 	 */
 	private fun deleteContactData() {
 		// Retrieve the shared preferences for the contact settings.
-		val contactSharedPreferences = requireActivity().getSharedPreferences(CONTACT_PREFS, Context.MODE_PRIVATE)
-
+		val contactSharedPreferences =
+			requireActivity().getSharedPreferences(CONTACT_PREFS, Context.MODE_PRIVATE)
+		
 		// Edit the shared preferences to remove the contact details.
 		contactSharedPreferences.edit().apply {
 			// Remove the contact name, number, and message from the shared preferences.
@@ -173,7 +160,7 @@ class ProfileFragment : Fragment() {
 			// Commit the changes to the shared preferences.
 			apply()
 		}
-
+		
 		// Hide the contact card from the UI.
 		binding.contactCard.visibility = View.GONE
 		// Display the attention card to indicate the absence of contact details.
@@ -221,68 +208,25 @@ class ProfileFragment : Fragment() {
 		}
 	}
 	
-	/**
-	 * Fetches the entry count for a specific day.
-	 *
-	 * @param dayKey The key representing the day.
-	 * @return The entry count for the day.
-	 */
-	private fun getEntryCountForDay(dayKey: String): Int {
-		return countPref?.getInt(dayKey, 0) ?: 0
+	private fun shouldFetchData(): Boolean {
+		val currentTime = LocalDateTime.now()
+		if (lastFetchTime == null || Duration.between(lastFetchTime, currentTime).toHours() >= 1) {
+			lastFetchTime = currentTime
+			Log.d(PROFILE_TAG, "shouldFetchData result: true")
+			return true
+		}
+		Log.d(PROFILE_TAG, "shouldFetchData result: false")
+		return false
 	}
 	
-	/**
-	 * Generates chart data for the last 60 days.
-	 */
-	private fun generateChartData() {
-		// Get the current date instance.
-		val calendar = Calendar.getInstance()
-
-		// Initialize lists to store data for the bar and line charts.
-		val barData = mutableListOf<Any>()
-		val lineData = mutableListOf<Any>()
-
-		// Loop through the last 30 days to generate data for the bar chart.
-		for (i in 0 until 30) {
-			// Create a unique key for each day in the format "day-month-year".
-			val dayKey = "${calendar.get(Calendar.DAY_OF_MONTH)}-${calendar.get(Calendar.MONTH) + 1}-${calendar.get(Calendar.YEAR)}"
-			
-			// Add the entry count for the day to the beginning of the barData list.
-			barData.add(0, getEntryCountForDay(dayKey))
-			
-			// Move the calendar date one day back.
-			calendar.add(Calendar.DAY_OF_YEAR, -1)
+	private fun getDatesInRange(startDate: LocalDate, endDate: LocalDate): List<String> {
+		val dates = mutableListOf<String>()
+		var currentDate = startDate
+		while (!currentDate.isAfter(endDate)) {
+			dates.add(currentDate.toString())
+			currentDate = currentDate.plusDays(1)
 		}
-
-		// Loop through the previous 30 days (days 31 to 60) to generate data for the line chart.
-		for (i in 0 until 30) {
-			// Create a unique key for each day in the format "day-month-year".
-			val dayKey = "${calendar.get(Calendar.DAY_OF_MONTH)}-${calendar.get(Calendar.MONTH) + 1}-${calendar.get(Calendar.YEAR)}"
-			
-			// Add the entry count for the day to the beginning of the lineData list.
-			lineData.add(0, getEntryCountForDay(dayKey))
-			
-			// Move the calendar date one day back.
-			calendar.add(Calendar.DAY_OF_YEAR, -1)
-		}
-
-		// Update the LiveData objects with the generated data.
-		barDataLiveData.value = barData.toTypedArray()
-		lineDataLiveData.value = lineData.toTypedArray()
-	}
-	
-	/**
-	 * Initializes the data for the bar and line charts.
-	 */
-	private fun setupChartData() {
-		// Retrieve the bar data from LiveData; default to an empty array if null.
-		val barData = barDataLiveData.value ?: arrayOf()
-
-		// Retrieve the line data from LiveData; default to an empty array if null.
-		val lineData = lineDataLiveData.value ?: arrayOf()
-
-		// Set up the chart with the retrieved bar and line data.
-		setupChartData(barData, lineData)
+		return dates
 	}
 	
 	/**
@@ -290,33 +234,242 @@ class ProfileFragment : Fragment() {
 	 *
 	 * @param count The count of entries for the day.
 	 */
-	private fun getCountEntryToFirestore() {
-		val collection = Firebase.auth.currentUser?.let { db.collection(it.uid) }
-		collection?.document("${LocalDate.now()}")?.get()!!.addOnSuccessListener {
-			Log.d(TAG, "DocumentSnapshot data: ${it.data?.get("count")}") }
+	private fun getCountEntryFromFirestore() {
+		Log.d(PROFILE_TAG, "getCountEntryFromFirestore started")
+		if (!shouldFetchData()) return
+		
+		val currentUser = Firebase.auth.currentUser ?: return
+		if (currentUser == null) {
+			Log.d(PROFILE_TAG, "Current user is null")
+			return
+		}
+		val collection = db.collection(currentUser.uid)
+		
+		val endDate = LocalDate.now()
+		val startDate = endDate.minusDays(60)
+		val dateRange = getDatesInRange(startDate, endDate)
+		
+		val fetchedData = mutableListOf<Int>()
+		var fetchedCount = 0
+		
+		for (date in dateRange) {
+			collection.document(date).get().addOnSuccessListener { document ->
+				if (document.exists()) {
+					document.get("count")?.let {
+						if (it is Int) {
+							fetchedData.add(it)
+							Log.d(PROFILE_TAG, "Date: $date, Count: $it")
+							
+						}
+					}
+				} else {
+					fetchedData.add(0)
+				}
+				fetchedCount++
+				if (fetchedCount == dateRange.size) {
+					when {
+						fetchedData.size >= 60 -> {
+							barChart.addAll(fetchedData.takeLast(30))
+							lineChart.addAll(fetchedData.take(30))
+						}
+						
+						fetchedData.size in 30..59 -> {
+							barChart.addAll(fetchedData.takeLast(30))
+						}
+						
+						else -> {
+							barChart.clear()
+							lineChart.clear()
+						}
+					}
+					setupChartData(barChart.toTypedArray(), lineChart.toTypedArray())
+				}
+			}.addOnFailureListener { exception ->
+				Log.e(PROFILE_TAG, "Error getting document for date: $date", exception)
+			}
+		}
+		Log.d(PROFILE_TAG, "getCountEntryFromFirestore finished")
 	}
+	
 	
 	/**
 	 * Define and set up chart data.
 	 * This method initializes the data for the bar and line charts and configures the chart's appearance and behavior.
 	 */
 	private fun setupChartData(barData: Array<Any>, lineData: Array<Any>) {
-		// Set the bar series and line series with their respective parameters.
-		val barSeriesElement = AASeriesElement().name("Last 30 days").color("#FFDAD6").type(AAChartType.Column).data(barData)
-		val lineSeriesElement = AASeriesElement().name("Previous 30 days").color("#FFE088").type(AAChartType.Line).data(lineData)
+		if (barData.isEmpty() && lineData.isEmpty()) {
+			binding.aaChartView.visibility = View.GONE
+			binding.emptyDataTextView.visibility = View.VISIBLE
+			binding.emptyDataTextView1.visibility = View.VISIBLE
+			binding.emptyDataTextView2.visibility = View.VISIBLE
+			return
+		}
 		
-		// Create an AAChartModel with the desired settings.
+		binding.aaChartView.visibility = View.VISIBLE
+		binding.emptyDataTextView.visibility = View.GONE
+		binding.emptyDataTextView1.visibility = View.GONE
+		binding.emptyDataTextView2.visibility = View.GONE
+		
+		val seriesList = mutableListOf<AASeriesElement>()
+		seriesList.add(
+			AASeriesElement().name("Current month").color("#FFDAD6").type(AAChartType.Column)
+				.data(barData)
+		)
+		
+		if (lineData.isNotEmpty()) {
+			seriesList.add(
+				AASeriesElement().name("Previous month").color("#FFE088").type(AAChartType.Line)
+					.data(lineData)
+			)
+		}
+		
 		val aaChartModel = AAChartModel()
 			.chartType(AAChartType.Column)
 			.title(getString(R.string.your_gratitude_note_activity))
 			.backgroundColor("#00000000")
 			.dataLabelsEnabled(true)
-			.series(arrayOf(barSeriesElement, lineSeriesElement))
+			.series(seriesList.toTypedArray())
 			.yAxisTitle("Amount of notes per day")
 			.axesTextColor("#FFFFFF")
 			.titleStyle(AAStyle().color("#FFFFFF"))
 		
-		// Draw the chart in the aaChartView using the aaChartModel.
 		binding.aaChartView.aa_drawChartWithChartModel(aaChartModel)
+	}
+	
+	/**
+	 * Save dummy data for the last 60 days to Firestore.
+	 */
+	private fun save60DummyDataToFirestore() {
+		val currentUser = Firebase.auth.currentUser ?: return
+		val collection = db.collection(currentUser.uid)
+		
+		val endDate = LocalDate.now()
+		val startDate = endDate.minusDays(59)
+		val dateRange = getDatesInRange(startDate, endDate)
+		
+		// Dummy data values for the last 60 days.
+		val dummyDataValues = listOf(
+			2,
+			1,
+			2,
+			1,
+			2,
+			1,
+			2,
+			1,
+			3,
+			2,
+			1,
+			3,
+			4,
+			2,
+			1,
+			4,
+			5,
+			3,
+			2,
+			1,
+			6,
+			4,
+			3,
+			2,
+			6,
+			5,
+			4,
+			3,
+			7,
+			6,
+			5,
+			4,
+			8,
+			7,
+			6,
+			8,
+			9,
+			7,
+			6,
+			10,
+			8,
+			7,
+			10,
+			11,
+			9,
+			8,
+			12,
+			11,
+			10,
+			12,
+			13,
+			11,
+			10,
+			13,
+			14,
+			12,
+			11,
+			14,
+			15,
+			13,
+			12
+		)
+		
+		for (i in dateRange.indices) {
+			val date = dateRange[i]
+			val count = dummyDataValues[i]
+			val data = hashMapOf("count" to count)
+			collection.document(date).set(data)
+		}
+	}
+	
+	/**
+	 * Save dummy data for the last 30 days to Firestore.
+	 */
+	private fun save30DummyDataToFirestore() {
+		val currentUser = Firebase.auth.currentUser ?: return
+		val collection = db.collection(currentUser.uid)
+		
+		val endDate = LocalDate.now()
+		val startDate = endDate.minusDays(29)
+		val dateRange = getDatesInRange(startDate, endDate)
+		
+		// Dummy data values for the last 30 days.
+		val dummyDataValues = listOf(
+			2,
+			1,
+			2,
+			1,
+			2,
+			1,
+			2,
+			1,
+			3,
+			2,
+			1,
+			3,
+			4,
+			2,
+			1,
+			4,
+			5,
+			3,
+			2,
+			1,
+			6,
+			4,
+			3,
+			2,
+			6,
+			5,
+			4,
+			3,
+			7,
+			6
+		)
+		
+		for (i in dateRange.indices) {
+			val date = dateRange[i]
+			val count = dummyDataValues[i]
+			val data = hashMapOf("count" to count)
+			collection.document(date).set(data)
+		}
 	}
 }
